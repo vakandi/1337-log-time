cdc56e9eafa39bd322b207c6cfa6de90a11a3161
vakandi <wael.bousfira@protonmail.com>
2022-12-11 01:34:43 +0100

    update

--- get_locations.rb
+++ get_locations.rb
@@ -6,8 +6,8 @@ token = client.client_credentials.get_token
 require 'date'
 d = Date.today
 response = token.get("/v2/users/" + ARGV[0] + "/locations_stats?begin_at=" + d.prev_month.strftime("%Y-%m") +"-28")
+#The date is updating automatically every month, from 28th of the previous month to 28th on the current one accordly to the Scholarship
 response.status
-# => 200
+
 response.parsed
 puts response.inspect
-# => {"id"=>1, "name"=>"Paris", "time_zone"=>"Europe/Paris", "language"=>{"id"=>1, "name"=>"Français", "identifier"=>"fr"}, "users_count"=>22997, "vogsphere_id"=>1, "country"=>"France", "address"=>"96, boulevard Bessières", "zip"=>"75017", "city"=>"Paris", "website"=>"http://www.42.fr/", "facebook"=>"https://facebook.com/42born2code", "twitter"=>"https://twitter.com/42born2code", "active"=>true, "public"=>true, "email_extension"=>"42.fr", "default_hidden_phone"=>false, "endpoint"=>{"id"=>33, "url"=>"https://endpoint.42paris.fr", "description"=>"42paris's new ldapkdc endpoint", "created_at"=>"2021-06-14T07:49:14.872Z", "updated_at"=>"2021-06-28T15:24:45.089Z"}}
