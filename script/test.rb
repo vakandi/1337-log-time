c30fef0fb77b92ad62431295e8d65d8500b8a5bd
wbousfi <wael.bousfira@protonmail.com>
2023-01-27 01:41:14 +0100

    update22189

--- script/test.rb
+++ script/test.rb
@@ -1,13 +1,27 @@
-require 'json'
-require 'time'
+require "oauth2"
+UID= "5f098420ac985c69454de207c0ed775aa09f12c7e1e2dc7d7070dd6ec27d49da" #public key here
+SECRET= "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" #SECRET KEY HERE
+client = OAuth2::Client.new(UID, SECRET, site: "https://api.intra.42.fr")
+token = client.client_credentials.get_token
+require 'date'
+d = Date.today
+response = token.get("/v2/users/" + ARGV[0] + "/locations_stats?begin_at=" + d.prev_month.strftime("%Y-%m") +"-28")
+#The date is updating automatically every month, from 28th of the previous month to 28th on the current one accordly to the Scholarship
+response.status
 
-response_body = "{\"202194\",\"202"
+api_output_json = response.parsed
 
-response_hash = JSON.parse(response_body)
+daily_hours = {}
 
-response_hash.each do |date, time|
-  time_in_seconds = Time.parse(time).seconds_since_midnight
-  hours = time_in_seconds / 3600
-  puts "#{date}: #{hours} hours"
+api_output_json.each do |date, time_string|
+    # Parse the time string into hours, minutes, and seconds
+    hours, minutes, seconds = time_string.split(':').map(&:to_i)
+    # Add the total number of hours for the day to the `daily_hours` hash
+    daily_hours[date] = hours + (minutes / 60.0) + (seconds / 3600.0)
+end
+
+# Print out the results
+daily_hours.each do |date, hours|
+    puts "#{date}: #{hours} hours"
 end
 
