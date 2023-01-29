eb07a4a4ef01277fbc4336419e1b392d4ced889c
vakandi <wael.bousfira@protonmail.com>
2023-01-29 04:11:16 +0000

    update

--- update_ngrok.sh
+++ update_ngrok.sh
@@ -10,8 +10,10 @@ do
 		nohup ngrok http 80 &
 		sleep 4s
 		new_url="$(curl -s localhost:4040/api/tunnels | jq -r .tunnels\[0\].public_url)"
-		echo "$(cat index.html | sed "s|\(url=\).*\(\"\)|\1$new_url\2|")" > index.html
-		echo "$(cat README.md | sed "s|\(url=\).*\(\>\)|\1$new_url\2|")" > README.md
+		old_link_index="$(grep -o 'url=.*"' index.html | cut -c5- | sed 's/"//g')"
+		old_link_readme="$(grep -o 'url> .*' README.md | cut -c6-)"
+		perl -i -pe "s|$old_link_readme|$new_url|g" README.md
+		perl -i -pe "s|$old_link_index|$new_url|g" index.html
 		git add index.html README.md
 		git commit -m "ngrok_newlink"
 		git push origin main
