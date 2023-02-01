9835743b74b1274d5635d7aaa521eae323791818
vakandi <wael.bousfira@protonmail.com>
2023-02-01 19:27:04 +0000

    update

--- index.php
+++ index.php
@@ -96,6 +96,14 @@ input[type="submit"]:active {
     color: white;
     margin-top: 100px;
 }
+.github-repo {
+    font-family: 'Anonymous Pro';
+    font-size: 50px;
+    text-align: center;
+    color: white;
+position: relative;
+top: 65%; bottom: 100px;
+} 
 
 .purple-link {
     color: purple;
@@ -103,7 +111,61 @@ input[type="submit"]:active {
 }
 @media only screen and (min-width: 980px) {
 pre {
+font-size: 20px;
 bottom: 20%;
+}
+form {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 20%;
+    margin: 0 auto;
+    background-size: cover;
+    background-position: center;
+}
+
+label {
+    font-size: 25px;
+    color: white;
+    font-family: 'Anonymous Pro';
+}
+
+input[type="text"] {
+    font-size: 20px;
+    padding: 20px;
+    margin-bottom: 20px;
+    width: 60%;
+    border-radius: 15px;
+    margin-right: 15px;
+    margin-top: 20px;
+    font-family: 'Anonymous Pro';
+}
+
+input[type="submit"] {
+    background-color: green;
+    color: white;
+    font-size: 20px;
+    padding: 20px;
+    width: 60%;
+    cursor: pointer;
+    border-radius: 15px;
+    margin-left: 17%;
+    font-family: 'Anonymous Pro';
+    display: flex;
+    justify-content: center;  
+    align-items: center;
+}
+
+.title {
+    font-size: 20px;
+    margin-top: 100px;
+}
+.github-repo {
+    font-size: 20px;
+position: relative;
+top: 65%; bottom: 100px;
+}
 }
 </style>
 <body>
@@ -113,7 +175,9 @@ bottom: 20%;
         <input type="text" id="login" name="login">
 <input type="submit" id="submit-button" value="GET LOGTIME">
 </form>
-                                                                            <?php
+
+
+<?php
 if(isset($_POST["login"])) {
     require "vendor/autoload.php";
 $provider = new League\OAuth2\Client\Provider\GenericProvider([
@@ -155,5 +219,6 @@ try {                                                                           
 }
 ?>
 
+<div class="github-repo"><a href="https://github.com/vakandi/1337-log-time" class="purple-link">GitHub Repo</a></div>
 </body>
 </html>
