51376d64c5003baefe738dcda5d8ac2a6322dd02
vakandi <wael.bousfira@protonmail.com>
2024-11-12 07:59:50 +0100

    update

--- get_logtime.rb
+++ get_logtime.rb
@@ -1,10 +1,17 @@
 require "oauth2"
-UID= "**********"
-SECRET= "s-s***********" #SECRET KEY HERE
+UID= "5f098420ac985c69454de207c0ed775aa09f12c7e1e2dc7d7070dd6ec27d49da"
+
+SECRET= "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"
 client = OAuth2::Client.new(UID, SECRET, site: "https://api.intra.42.fr")
 token = client.client_credentials.get_token
 require 'date'
 d = Date.today
+#response = token.get("/v2/users/" + ARGV[0] + "/locations_stats?begin_at=" + d.prev_month.strftime("%Y-%m") +"-28")
+# perform the rrquest but by fixing the error : `+': no implicit conversion of nil into String (TypeError) 
+if ARGV[0] == nil
+    puts "Please enter a login"
+    exit
+end
 response = token.get("/v2/users/" + ARGV[0] + "/locations_stats?begin_at=" + d.prev_month.strftime("%Y-%m") +"-28")
 #The date is updating automatically every month, from 28th of the previous month to 28th on the current one accordly to the Scholarship
 response.status
@@ -34,5 +41,172 @@ daily_hours.each do |date, hours|
    # puts "#{date}: #{hours.to_i} hours and #{minutes.to_i} minutes"
 end
 puts "\033[1;42m" + "  Total :[ #{total_hours.to_i} h ] " + "\033[0m"
-puts "\033[1;32m" + "Average hours: #{(total_hours.to_i / daily_hours.length).to_i} h" + "\033[0m"
+puts "\033[1;32m" + "Average hours per day: #{(total_hours.to_i / daily_hours.length).to_i} h" + "\033[0m"
 
+
+
+
+
+# ASCII art digits
+DIGITS = {
+  "0" => [
+    "    ████████    ",
+    "  ██        ██  ",
+    "██            ██",
+    "██            ██",
+    "██            ██",
+    "██            ██",
+    "██            ██",
+    "██            ██",
+    "██            ██",
+    "  ██        ██  ",
+    "    ████████    "
+  ],
+  "1" => [
+    "      ████      ",
+    "    ██████      ",
+    "      ████      ",
+    "      ████      ",
+    "      ████      ",
+    "      ████      ",
+    "      ████      ",
+    "      ████      ",
+    "      ████      ",
+    "      ████      ",
+    "  ████████████  "
+  ],
+  "2" => [
+    "    ████████    ",
+    "  ██        ██  ",
+    "██            ██",
+    "              ██",
+    "          ████  ",
+    "      ████      ",
+    "  ████          ",
+    "██              ",
+    "██              ",
+    "██            ██",
+    "  ██████████████"
+  ],
+  "3" => [
+    "    ████████    ",
+    "  ██        ██  ",
+    "██            ██",
+    "              ██",
+    "        ████████",
+    "              ██",
+    "              ██",
+    "              ██",
+    "██            ██",
+    "  ██        ██  ",
+    "    ████████    "
+  ],
+  "4" => [
+    "██            ██",
+    "██            ██",
+    "██            ██",
+    "██            ██",
+    "██            ██",
+    "  ██████████████",
+    "              ██",
+    "              ██",
+    "              ██",
+    "              ██",
+    "              ██"
+  ],
+  "5" => [
+    "██████████████  ",
+    "██              ",
+    "██              ",
+    "██              ",
+    "  ████████████  ",
+    "              ██",
+    "              ██",
+    "              ██",
+    "██            ██",
+    "  ██        ██  ",
+    "    ████████    "
+  ],
+  "6" => [
+    "    ████████    ",
+    "  ██        ██  ",
+    "██              ",
+    "██              ",
+    "██  ████████    ",
+    "████        ██  ",
+    "██            ██",
+    "██            ██",
+    "██            ██",
+    "  ██        ██  ",
+    "    ████████    "
+  ],
+  "7" => [
+    "██████████████  ",
+    "              ██",
+    "            ██  ",
+    "          ██    ",
+    "        ██      ",
+    "      ██        ",
+    "    ██          ",
+    "  ██            ",
+    "██              ",
+    "██              ",
+    "██              "
+  ],
+  "8" => [
+    "    ████████    ",
+    "  ██        ██  ",
+    "██            ██",
+    "██            ██",
+    "  ██        ██  ",
+    "    ████████    ",
+    "  ██        ██  ",
+    "██            ██",
+    "██            ██",
+    "  ██        ██  ",
+    "    ████████    "
+  ],
+  "9" => [
+    "    ████████    ",
+    "  ██        ██  ",
+    "██            ██",
+    "██            ██",
+    "██            ██",
+    "  ██        ████",
+    "    ████████  ██",
+    "              ██",
+    "              ██",
+    "  ██        ██  ",
+    "    ████████    "
+  ]
+}
+
+def number_to_ascii(number)
+  digits = number.to_s.chars
+  height = DIGITS.first[1].length
+
+  # Create array to hold each line of output
+  result = Array.new(height) { "" }
+
+  # Build each line of the ASCII art
+  digits.each do |digit|
+    DIGITS[digit].each_with_index do |line, i|
+      result[i] += line + "  "
+    end
+  end
+
+  result.join("\n")
+end
+
+# Modified output lines using ASCII art
+puts "\033[1;42m"
+puts "Total hours:"
+#print 'h' next to the number and not below it, so on the same line
+#puts number_to_ascii(total_hours.to_i)
+puts number_to_ascii(total_hours.to_i) + "H"
+puts "\033[0m"
+
+puts "\033[1;32m"
+puts "Average hours per day:"
+puts number_to_ascii((total_hours.to_i / daily_hours.length).to_i) + "H"
+puts "\033[0m"
